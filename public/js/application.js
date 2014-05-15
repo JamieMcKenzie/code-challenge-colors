85c5b74798d25f5dae407c2874ee0d82a79666d3
Jamie McKenzie <jam@jamies-mbp.att.net>
2014-05-15 11:42:07 -0700

    Begins implementation of save button, identifies li items by inde and iterates using $.each.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
+var colors = ['red', 'blue', 'white', 'red', 'blue', 'white', 'red', 'blue', 'white', 'red', 'blue', 'white', 'red', 'blue', 'white', 'red']
+
+// for (var i = 0; i < colors.size; i++){
+//   $("li:nth-child(i)").css("background-color", colors[i]);
+// }
+
 $(document).ready(function() {
   $('#clear').on('click', function(e){
     e.preventDefault();
     $("li:nth-child(n)").css("background-color", 'white');
+  });
+
+  $('#save').on('click', function(e){
+    e.preventDefault();
+    $('li').each(function(index){
+      $('li:nth-child(' + index + ')').css("background-color", colors[index])
+    })
   });
 
   $('li').on('click', function(e){
@@ -17,3 +30,5 @@ $(document).ready(function() {
     });
   });
 });
+
+
